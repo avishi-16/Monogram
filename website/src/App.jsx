3296188a53494f0c74e8968a297ae8d06190bb1b
avishi-16 <avishisharma1603@gmail.com>
2023-09-18 11:11:37 +0530

    Initial commit

---
+++ website/src/App.jsx
@@ -0,0 +1,96 @@
+import './App.css';
+function App() {
+
+  return (
+    <div>
+
+    <div className='first'>
+    <header>
+    <img src= "https://o.remove.bg/downloads/996c262c-5334-4245-8649-34082e65feee/360_F_318128320_CNZWWxvqVKqEXKoxvu70LYl3iRBjuK4w-removebg-preview.png"/>
+    <span>MONOGRAM</span>
+    <ul>
+      <li>How it works</li>
+      <li>Workflows</li>
+      <li>Download</li>
+      <li>Blog</li>
+      <li>Shop</li>
+    </ul>
+    </header>
+    <h1>
+      A  CONSOLE  FOR {"\n"}
+      <span>EVERY  WORKFLOW</span>
+    </h1>
+    <p>Discover the perfect console for yours.</p>
+    </div>
+
+    <div className='sec'>
+      <img src = "https://images.reverb.com/image/upload/s--rBl4VrRy--/f_auto,t_large/v1646172925/a5gtc4ykurgibsubst0z.jpg"/>
+      <img src ="https://images.reverb.com/image/upload/s--bl1qmmTv--/f_auto,t_large/v1653268355/f9vyzcxdjrs6ek0p5zuo.jpg"/>
+    </div>
+    <div className='det'>
+      <h4>AUDIO CONSOLE  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $ 250</h4>
+      <h4>MINI CONSOLE  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $ 250</h4>
+    </div>
+    <div className='three'>
+      <img src ="https://imgs.search.brave.com/VJtHFPkJcMkny8Z9Jwb2QLZl77V2PKzEaUZJaBywgQ0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9oaXBz/LmhlYXJzdGFwcHMu/Y29tL2htZy1wcm9k/L2ltYWdlcy9naC1p/bmRleC1nYW1pbmdj/b25zb2xlcy0wNTIt/cHJpbnQtcHJldmll/dy0xNjU5NzA1MTQy/LmpwZz9yZXNpemU9/MTIwMDoq"/>
+      <img src ="https://imgs.search.brave.com/a5zpbCwbii2LDXnw9WQVwLMSUe_Pw0xNSk0wiTtf91E/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tb25v/Z3JhbWNjLmNvbS9z/dGF0aWMvMTA1NTZh/NDBlM2YwZWE1NTVk/Y2U2ZTA1YjI2Nzhm/YmQvZWQ2MjkvU1df/Ym94X3JlbmRlcl9w/aG90b19wb3J0cmFp/dF85MTQ2YzI5ZTI4/LmpwZw"/>
+    </div>
+    <div className='det2'>
+      <h4>GAMING CONSOLE  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $ 250</h4>
+      <h4>PHOTO CONSOLE  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; $ 250</h4>
+    </div>
+    <div className='four'>
+      <img src ="https://imgs.search.brave.com/Gwv7dvSUPGGfDetd8SLvwqtee-exwPZdGdOMr4bREIY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9rc3It/dWdjLmltZ2l4Lm5l/dC9hc3NldHMvMDI2/LzM4Ni8yMjgvZjVj/NzBmODc2YmEwNDgy/MzVhYTJjMTg5OTU5/MjQ3ZjFfb3JpZ2lu/YWwuanBnP2l4bGli/PXJiLTQuMC4yJmNy/b3A9ZmFjZXMmdz0x/MDI0Jmg9NTc2JmZp/dD1jcm9wJnY9MTU2/Nzc4OTYwOCZhdXRv/PWZvcm1hdCZmcmFt/ZT0xJnE9OTImcz1k/OGM2ZTI1ZmIzOTMx/N2M2ZmNiNTcyN2Q0/ODc5M2Y5ZQ"/>
+      <img src ="https://imgs.search.brave.com/puOU6peKsjI5Agc675faIxsvQI3JnYbafvfbo5ZkGKc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90aGVn/YWRnZXRmbG93LmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MS8wNi9Nb25vZ3Jh/bS1DcmVhdGl2ZS1D/b25zb2xlLU1vZHVs/YXItQ29udHJvbC1D/b2xsZWN0aW9uLTAx/LTEyMDB4OTAwLmpw/Zw"/>
+      <img src ="https://imgs.search.brave.com/L2mdMfOqsnLy6VC5XMrzl3dKqqZzwmCU8VEURxlidJc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzQ2LzA3/LzIwLzQ2MDcyMDA2/ZWI0NzNkMmY1NjUw/ZWVmNmFiYjA5OTli/LmpwZw"/>
+    </div>
+    <div className='det3'>
+      <h4>AUDIO CONSOLE  </h4>
+      <h4>VIDEO CONSOLE </h4>
+      <h4>PHOTO CONSOLE  </h4>
+    </div>
+    <div className='five'>
+      <img src ="https://imgs.search.brave.com/Gwv7dvSUPGGfDetd8SLvwqtee-exwPZdGdOMr4bREIY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9rc3It/dWdjLmltZ2l4Lm5l/dC9hc3NldHMvMDI2/LzM4Ni8yMjgvZjVj/NzBmODc2YmEwNDgy/MzVhYTJjMTg5OTU5/MjQ3ZjFfb3JpZ2lu/YWwuanBnP2l4bGli/PXJiLTQuMC4yJmNy/b3A9ZmFjZXMmdz0x/MDI0Jmg9NTc2JmZp/dD1jcm9wJnY9MTU2/Nzc4OTYwOCZhdXRv/PWZvcm1hdCZmcmFt/ZT0xJnE9OTImcz1k/OGM2ZTI1ZmIzOTMx/N2M2ZmNiNTcyN2Q0/ODc5M2Y5ZQ"/>
+      <img src ="https://imgs.search.brave.com/puOU6peKsjI5Agc675faIxsvQI3JnYbafvfbo5ZkGKc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90aGVn/YWRnZXRmbG93LmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MS8wNi9Nb25vZ3Jh/bS1DcmVhdGl2ZS1D/b25zb2xlLU1vZHVs/YXItQ29udHJvbC1D/b2xsZWN0aW9uLTAx/LTEyMDB4OTAwLmpw/Zw"/>
+      <img src ="https://imgs.search.brave.com/L2mdMfOqsnLy6VC5XMrzl3dKqqZzwmCU8VEURxlidJc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzQ2LzA3/LzIwLzQ2MDcyMDA2/ZWI0NzNkMmY1NjUw/ZWVmNmFiYjA5OTli/LmpwZw"/>
+    </div>
+    <div className='det4'>
+      <h4>AUDIO CONSOLE  </h4>
+      <h4>VIDEO CONSOLE </h4>
+      <h4>PHOTO CONSOLE  </h4>
+    </div>
+    <footer>
+      <ul>
+        <li>MONOGRAM</li>
+        <li>QUICK LINKS</li>
+        <li>HELP & SUPPORT</li>
+        <li>INFORMATION</li>
+        <li>SUBSCRIBE</li>
+      </ul>
+      <div>
+        <p>305,King St W. Suite,Canada</p>
+        <ul>
+          <li>Home</li>
+          <li>How it Works</li>
+          <li>Shop</li>
+          <li>Download</li>
+        </ul>
+        <ul>
+          <li>FAQ</li>
+          <li>Support Center</li>
+        </ul>
+        <ul>
+          <li>About Us</li>
+          <li>Privacy Policy</li>
+        </ul>
+       <input type="email" placeholder="Enter your email here" id="email" className='box'>
+          </input>
+          <button>SUBMIT</button>
+      </div>
+      <p className='last'>&copy; Monogram 2023</p>
+    </footer>
+    </div>
+  )
+}
+
+export default App
